876bef50f5eb77af968d5659c9a22ff106525c5b
pakhomov <pakhomov.o@donnu.edu.ua>
2020-09-16 14:04:13 +0300

    commit4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,17 +12,26 @@ namespace ConsoleApp1
         {
             /*Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine("Привіт ДоннУ");*/
-            string name = "Tom";
+            /*string name = "Tom";
                 int age = 33;
                 bool isEmployed = false;
-                double weight = 78.65;
+                double weight = 78.65;*/
 
             /*Console.WriteLine($"Имя: {name}");
             Console.WriteLine($"Возраст: {age}");
             Console.WriteLine($"Вес: {weight}");
             Console.WriteLine($"Работает: {isEmployed}");*/
-            Console.WriteLine("Имя: {0}  Возраст: {1}  Рост: {2}", name, age, weight);
-            Console.WriteLine($"Имя: {name}  Возраст: {age}  Рост: {weight}");
+            /*Console.WriteLine("Имя: {0}  Возраст: {1}  Рост: {2}", name, age, weight);
+            Console.WriteLine($"Имя: {name}  Возраст: {age}  Рост: {weight}");*/
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите рост: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите размер зарплаты: ");
+            decimal salary = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($"Имя: {name}  Возраст: {age}  Рост: {height}м  Зарплата: {salary}$");
             Console.ReadKey();
         }
     }
